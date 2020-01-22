2bdb7b71e29538c2c623eadd7e474e2700b71a50
huainanzi <huainanzi@wacai.com>
2020-01-22 18:33:41 +0800

    添加日志配置 并且验证

--- src/main/java/com/zwh/o2o/web/superadmin/AreaController.java
+++ src/main/java/com/zwh/o2o/web/superadmin/AreaController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,13 +20,16 @@ import com.zwh.o2o.service.AreaService;
 @RequestMapping("/superadmin")
 public class AreaController {
 	
+	Logger logger = LoggerFactory.getLogger(AreaController.class);
+	
 	@Autowired
 	private AreaService areaService;
 	
 	@RequestMapping(value = "/listareas", method = RequestMethod.GET)
 	@ResponseBody // JSON格式
 	private Map<String, Object> listAreas() {
-
+		logger.info("------- listAreas start -------");
+		long startTime = System.currentTimeMillis();
 		Map<String, Object> modelMap = new HashMap<String, Object>();
 		List<Area> list = new ArrayList<Area>();
 		try {
@@ -36,7 +41,12 @@ public class AreaController {
 			modelMap.put("success", false);
 			modelMap.put("errMsg", e.toString());
 		}
-		return modelMap;
 		
+		logger.error("------- listAreas test error -------");
+		
+		long endTime = System.currentTimeMillis();
+		logger.debug("listAreas costTime --->", endTime - startTime);
+		logger.info("------- listAreas start -------");
+		return modelMap;
 	}
 }
